11394847bde43179d669789676f3bfc6b3cf1c47
Sri Naika <Srinivas.Naika@gmail.com>
2021-05-08 18:11:08 +0530

    firstcommit

---
+++ src/test/java/com/qa/core/Base.java
@@ -0,0 +1,45 @@
+package com.qa.core;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+
+	public static WebDriver driver;
+
+	// Created by Srinivas Naika on 07/05/2021
+	// Updated by
+	// Method to do a Browser Setup
+	public WebDriver browserSetup() {
+
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver(); // Launch
+
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		driver.manage().deleteAllCookies(); // Delete all the cookies
+
+		driver.manage().window().maximize(); // Maximize the window
+
+		return driver;
+
+	}
+
+	// Created by Srinivas Naika on 07/05/2021
+	// Updated by
+	// Method to pass the site url
+	public void passSiteURL() {
+		driver.get("https://www.amazon.in/");
+	}
+
+	// Created by Srinivas Naika on 07/05/2021
+	// Updated by
+	// Method to do close the browser
+	public void browserClose() {
+		driver.close();
+	}
+
+}
---
+++ src/test/java/com/qa/test/HomePage.java
@@ -0,0 +1,35 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+import com.qa.core.Base;
+
+public class HomePage extends Base {
+	
+	
+	String ExpectedHomePageTitle= "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+	
+	
+//	Verify that when user lands on HomePage , Page Title is displayed and it should be 
+//	"Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in"
+	@Test
+	public void TC001()
+	{
+		 browserSetup();
+		 passSiteURL();
+		 String ActualHomePageTitle  =driver.getTitle();
+		 //do an assertion		 
+		 browserClose();
+		 
+	}
+	
+	@Test
+	public void TC002()
+	{
+		
+		
+	}
+	
+	
+
+}
