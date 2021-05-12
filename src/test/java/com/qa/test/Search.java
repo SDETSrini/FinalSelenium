5c496e4fca3e1d446ffe4692de02ad42c41b21dc
Sri Naika <Srinivas.Naika@gmail.com>
2021-05-12 19:59:39 +0530

    updating search.java

--- src/test/java/com/qa/test/Search.java
+++ src/test/java/com/qa/test/Search.java
@@ -1,15 +1,16 @@
 package com.qa.test;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.qa.core.Base;
 
 public class Search  extends Base{
 	
-	String ActualSearchResultPage = "Java | Test";
+	String ActualSearchResultPage = "Amazon.in : Java";
+
 	
 	
 	public void verifySearchResult()
